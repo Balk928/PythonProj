09f80ffb523dbec154156bc6aabe3dc91e7dbf68
bal krishna <balksharma337@gmail.com>
2021-04-23 11:25:54 +0530

    Changes some code

--- helo.py
+++ helo.py
@@ -1,1 +1,11 @@
-print("HelloGo")
+try:
+    print("line1")
+    try:
+        print("line2")
+        print("line3")
+    except ZeroDivisionError:
+        print("except 1")
+    finally:
+        print("finally1")
+except TypeError:
+    print("except2")
