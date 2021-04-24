9a0e011c402217c7ddf840c32ced9a7f72024a6a
Balk928 <74726558+Balk928@users.noreply.github.com>
2021-04-24 22:18:16 +0530

    Update cpi.py

--- cpi.py
+++ cpi.py
@@ -21,6 +21,7 @@ t2.marks(70,70,70)
 print("Daa:",t2.Daa,"CoreJava:",t2.coreJava,"FlA:",t2.FlA)
 z = t2.Daa+t2.coreJava+t2.FlA//3
 print("Average Marks is :",z)
+print("-------------------Employee Details------------------")
 t3 = Student()
 t3.f3(input("EmployeeId:"),input("Enter Name:"),float(input("Salary:")))
 print(t3.emp_id,t3.Emp_Name,t3.Salary)
